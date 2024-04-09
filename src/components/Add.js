a041e676af68dd297ec24c84fea1fc4559802053
rikrdo pede <ricardopedemonte2018@gmail.com>
2024-04-09 00:45:34 -0300

    useEffect medio pelo agregado

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,13 +1,15 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import FormElement from "./FormElement.js";
 import { useNavigate } from "react-router-dom";
 import { ElementContext } from "../context/ElementContext.js";
+import Swal from "sweetalert2";
 
 const Add = () => {
     const navigate = useNavigate();
 
     const { elements, setElements } = useContext(ElementContext);
     const [shouldRedirect, setShouldRedirect] = useState(false);
+    const [counter, setCounter] = useState(3);
 
     const handleOnSubmit = (element) => {
         console.log(element);
@@ -15,10 +17,32 @@ const Add = () => {
         setShouldRedirect(true);
     }
 
-    if (shouldRedirect) {
-        console.log("Redirecting...")
-        navigate("/");
-    }
+    useEffect(() => {
+        if (shouldRedirect && counter > 0) {
+            const timer = setTimeout(() => {
+                Swal.fire({
+                    title: "Elemento añadido",
+                    text: `Serás redirigido en ${counter} segundos.`,
+                    icon: "success",
+                    timer: 900,
+                    timerProgressBar: true,
+                    showConfirmButton: false
+                });
+                setCounter(counter - 1);
+            }, 1000);
+            return () => clearTimeout(timer);
+        } else if (shouldRedirect && counter === 0) {
+            Swal.fire({
+                title: "Elemento añadido",
+                text: "Redirigiendo...",
+                icon: "success",
+                timer: 1000,
+                timerProgressBar: true,
+                showConfirmButton: false
+            });
+            navigate("/");
+        }
+    }, [shouldRedirect, counter, navigate]);
 
     return (
         <React.Fragment>
